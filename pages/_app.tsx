cd7de2e993d71b68cdf9de317e06ff98a4eb3129
superior <joystonlasrado27@gmail.com>
2022-12-19 23:33:04 +0530

    deleted theme.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from '@chakra-ui/react'
 import "../styles/global/globals.css";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {},
+  fonts: {},
+  fontSizes: {},
+  breakpoints: {
+    sm: "48em",
+    md: "52em",
+    lg: "64em",
+    xl: "80em",
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
